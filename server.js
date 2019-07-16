a36a76290f8343aabdc33643a094f81349d65941
segelu <segunakomolafe5@gmail.com>
2019-07-16 21:14:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ if (errf){
 }else{
 
 var newId = 1;		
-var timeStamp = Math.floor(Date.now() / 1000);
+var timeStamp = to_timestamp(req.body.date_added, 'DD Mon YYYY');
 
 client.query("INSERT INTO cars(id,owner,created_on,manufacturer,model,price,state,status,body_type) VALUES('"+ newId +"', '" + req.body.email + "', '" + timeStamp + "', '" + req.body.manufacturer + "', '" + req.body.model + "', '" + req.body.price + "', '" + req.body.state + "', '" + req.body.status + "', '" + req.body.body_type + "') RETURNING id;", (err, resp) => {
 if (err){
