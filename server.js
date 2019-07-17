02eeee7e52a153a7e424540eddf21a02b5344d74
segelu <segunakomolafe5@gmail.com>
2019-07-17 09:50:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -144,9 +144,9 @@ if (errf){
 }else{
 
 var newId = 1;		
-var timeStamp = to_timestamp(Date.now()/1000.0);
+var timeStamp = Date.now()/1000.0;
 
-client.query("INSERT INTO cars(id,owner,created_on,manufacturer,model,price,state,status,body_type) VALUES('"+ newId +"', '" + req.body.email + "', '" + timeStamp + "', '" + req.body.manufacturer + "', '" + req.body.model + "', '" + req.body.price + "', '" + req.body.state + "', '" + req.body.status + "', '" + req.body.body_type + "') RETURNING id;", (err, resp) => {
+client.query("INSERT INTO cars(id,owner,created_on,manufacturer,model,price,state,status,body_type) VALUES('"+ newId +"', '" + req.body.email + "', '" + (to_timestamp(${Date.now()}/ 1000.0)) + "', '" + req.body.manufacturer + "', '" + req.body.model + "', '" + req.body.price + "', '" + req.body.state + "', '" + req.body.status + "', '" + req.body.body_type + "') RETURNING id;", (err, resp) => {
 if (err){
 datae['status'] = 404;
 datae['error'] = err.stack;
@@ -176,6 +176,7 @@ res.send(datae);
 });
 
 });
+
 
 
 myapp.post('/order/', function (req, res) {
