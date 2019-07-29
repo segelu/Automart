d557276bd65931accaa488b0ee731740f1a6bfbc
segelu <segunakomolafe5@gmail.com>
2019-07-29 10:27:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -401,7 +401,7 @@ res.send(datae);
 myapp.get('/car/:car-id/', function (req, res) {
 client.connect();
 
-var carid = req.body.params.car-id;
+var carid = req.params.body.car-id;
 
 var datae = {};
 
